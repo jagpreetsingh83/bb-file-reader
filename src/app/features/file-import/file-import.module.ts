37809c443dfd377124acd74a46954616105b333c
Jagpreet Singh <jagpreet.singh@capco.com>
2020-02-05 09:47:43 -0500

    added table component

--- src/app/features/file-import/file-import.module.ts
+++ src/app/features/file-import/file-import.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { TableComponent } from './components/table/table.component';
 import { FileImportRoutingModule } from './file-import-routing.module';
 import { FileImportComponent } from './file-import.component';
 
 @NgModule({
-  declarations: [FileImportComponent],
+  declarations: [FileImportComponent, TableComponent],
   imports: [SharedModule, FileImportRoutingModule]
 })
 export class FileImportModule {}
