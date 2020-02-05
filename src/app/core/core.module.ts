dd7fe93a3ff925452fe5c33c0f482ce3cdf9c2e3
Jagpreet Singh <jagpreet.singh@capco.com>
2020-02-05 10:27:18 -0500

    core store setup

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,28 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { environment } from '@env/environment';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { LoggerModule } from 'ngx-logger';
 
 import { ErrorHandlerModule } from './error-handler/error-handler.module';
+import { reducers } from './store/reducers';
 
 @NgModule({
   imports: [
     LoggerModule.forRoot(environment.logging),
-    ErrorHandlerModule.forRoot()
+    ErrorHandlerModule.forRoot(),
+    StoreModule.forRoot(reducers, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    })
   ]
 })
 export class CoreModule {
