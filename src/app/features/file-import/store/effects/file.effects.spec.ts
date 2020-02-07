c8409144bb95eedb45fe87422efa9684d6f038b2
Jagpreet Singh <jagpreet.singh@capco.com>
2020-02-06 23:38:31 -0500

    Test for effect

--- src/app/features/file-import/store/effects/file.effects.spec.ts
+++ src/app/features/file-import/store/effects/file.effects.spec.ts
@@ -1,5 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { FileAdapterService, FileRecordAdapter } from '@file/services/file-adapter.service';
+import { FileReaderService } from '@file/services/file-reader.service';
+import * as FileStore from '@file/store';
 import { provideMockActions } from '@ngrx/effects/testing';
+import { AppTestModule } from '@tests/app-test.module';
+import * as FILE_MOCK from '@tests/file-import/file-import.test.util';
+import { cold, hot } from 'jasmine-marbles';
 import { Observable } from 'rxjs';
 
 import { FileEffects } from './file.effects';
@@ -7,16 +13,67 @@ import { FileEffects } from './file.effects';
 describe('FileEffects', () => {
   let actions$: Observable<any>;
   let effects: FileEffects;
+  let reader: FileReaderService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FileEffects, provideMockActions(() => actions$)]
+      imports: [AppTestModule],
+      providers: [
+        FileEffects,
+        FileReaderService,
+        FileAdapterService,
+        FileRecordAdapter,
+        provideMockActions(() => actions$)
+      ]
     });
 
     effects = TestBed.get<FileEffects>(FileEffects);
+    reader = TestBed.get(FileReaderService);
   });
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  describe('readFile$', () => {
+    const input = FileStore.readFile({
+      payload: { file: FILE_MOCK.DATA_TRANSFER }
+    });
+
+    it('should dispatch readFileSuccess upon success', () => {
+      const output = FileStore.readFileSuccess({
+        payload: {
+          records: FILE_MOCK.FILE_RECORDS
+        }
+      });
+
+      actions$ = hot('-a', { a: input });
+
+      const expected = cold('--b', { b: output });
+
+      reader.read = jasmine
+        .createSpy()
+        .and.returnValue(cold('-a|', { a: FILE_MOCK.CSV_RECORDS }));
+
+      expect(effects.readFile$).toBeObservable(expected);
+      expect(reader.read).toHaveBeenCalledWith(FILE_MOCK.DATA_TRANSFER);
+    });
+
+    it('should dispatch readFileFailure upon error', () => {
+      const error = new Error('kboom');
+
+      const output = FileStore.readFileFailure({
+        error: error.message
+      });
+
+      actions$ = hot('-a', { a: input });
+
+      const expected = cold('--b', { b: output });
+
+      reader.read = jasmine.createSpy().and.returnValue(cold('-#|', {}, error));
+
+      expect(effects.readFile$).toBeObservable(expected);
+      expect(reader.read).toHaveBeenCalledWith(FILE_MOCK.DATA_TRANSFER);
+    });
   });
 });
